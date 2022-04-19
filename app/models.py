61161ebe1b731133935ecdad23ae2614c2a13f03
Notsofastmatey <mrsimonlaycock@gmail.com>
2022-04-19 16:17:12 +0100

    Completed ch4 (Database) inc Paper table

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    papers = db.relationship('Paper', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Paper(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    paper_name = db.Column(db.String(140))
+    created_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+            return '<Paper {}>'.format(self.paper_name)
